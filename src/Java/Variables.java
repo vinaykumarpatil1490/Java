1d7ddcc02f7858b002d7027a97817649aae48487
vinaykumarpatil1490 <vinaykumarpatil1490@gmail.com>
2026-02-02 21:51:04 +0530

    Git Variables

---
+++ src/Java/Variables.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte Num1 = 2;
+        int Num2 = 4;
+        short Num3 = 4;
+        long Num4 = 64582;
+        char Num5 = 'a';
+        boolean Num6 = false;
+        float Num7 = 4.5f;
+        double Num8 = 5.687d;
+
+        System.out.println("Value = " +Num1);
+        System.out.println("Value = " +Num2);
+        System.out.println("Value = " +Num3);
+        System.out.println("Value = " +Num4);
+        System.out.println("Value = " +Num5);
+        System.out.println("Value = " +Num6);
+        System.out.println("Value = " +Num7);
+        System.out.println("Value = " +Num8);
+
+    }
+}
