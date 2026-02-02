24266c5cd8ed01ab47122c674cdc0ad2f4aab831
vinaykumarpatil1490 <vinaykumarpatil1490@gmail.com>
2026-02-02 07:34:58 +0530

    Git comment

---
+++ src/Comment.java
@@ -0,0 +1,16 @@
+public class Comment {
+    public static void main(String[] args) {
+        //Comment check
+        /*This is multi line comment
+
+
+
+
+        */
+/**
+ System.out.println( "check");
+ System.out.println("Print");
+ */
+
+    }
+}
